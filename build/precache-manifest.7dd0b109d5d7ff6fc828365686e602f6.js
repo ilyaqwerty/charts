f38596fc66a06d1bd9f38ada0a1c22443f285785
ilyaqwerty <topmastertop97@gmail.com>
2019-06-07 05:59:37 +0300

    heroku build

---
+++ build/precache-manifest.7dd0b109d5d7ff6fc828365686e602f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91b2a6990cb887ad80e3466882daaaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4160f48f3d22a677c76f",
+    "url": "/static/js/2.c2dfbb24.chunk.js"
+  },
+  {
+    "revision": "39a28a121ff0e1c2f77a",
+    "url": "/static/js/main.52ec7d0f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
